3902bb418ae0390b0896ab1d58a8beb3c45754b5
Jesse Carter <rollsrock25@gmail.com>
2015-01-29 18:44:57 -0500

    Lab1
    Assigning Variables

---
+++ Lab1/ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            string firstName;
+            char middleinitial;
+            string lastName; 
+            string fullname;
+            int age;
+            bool iscitizen;
+            bool canvote;
+            int heightfeet;
+            double heightinches;
+            double totalheightcm;
+
+            firstName = "Jesse";
+            middleinitial = 'A';
+            lastName = "Carter";
+            fullname = firstName + middleinitial + "." + lastName;
+
+            age = 29;
+            heightfeet = 6;
+            heightinches = 10.5;
+            double totalinches = heightfeet * 12 + heightinches;
+            totalheightcm = 2.54 * totalinches;
+
+            iscitizen = true;
+            canvote = iscitizen && age >= 18; 
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
